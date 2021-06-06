3906c2f386738eb23e10cafd0b6a4c73dcb3873d
minhlong43 <dominhlong43@gmail.com>
2021-06-07 00:05:45 +0700

    add

--- src/app/common/short-cut/short-cut.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShortCutPipe } from './short-cut.pipe';
-
-describe('ShortCutPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShortCutPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
